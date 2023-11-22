a7cdc95bbc7288119a3abe76de1bc743c393d0c4
GitHub Action <action@github.com>
2023-11-22 18:14:49 +0000

    Template cleanup

--- src/main/kotlin/com/github/robertojudele/copy/MyBundle.kt
+++ src/main/kotlin/com/github/robertojudele/copy/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.robertojudele.copy
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/robertojudele/copy/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/robertojudele/copy/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.robertojudele.copy.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
